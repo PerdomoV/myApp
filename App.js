58623192ca7683331b287139459fc2b210ef9081
Jorge Andres Perdomo Velasquez <japerdomov@Jorges-Mac-mini.local>
2024-03-18 13:29:28 -0500

    Se agregó un DrawerNavigator

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
 import NavigationStack from './src/navigation/NavigationStack';
 import NavigationTab from './src/navigation/NavigationTab';
+import NavigationDrawer from './src/navigation/NavigationDrawer';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <NavigationTab />
+      <NavigationDrawer/>
     </NavigationContainer>
   );
 }
